c747dfffb8537417b0c5de7e5e9d2eb45c212960
Sean Simpson <ssimpson@eastsidegamestudio.com>
2022-09-14 12:22:42 -0300

    Initial attempt
    I expect failure!
    
    Forgot to add the client.sln to the root
    
    fixed path to csproj
    
    Fixed sonarscanner begin and csproj
    
    Fixed action key/org; added ReferencePath to csproj
    
    Fixed ReferencePath for hard-coded value
    
    Created Editor script for dynamic csproj creation
    
    Fixed compile include
    
    Added project base dir to attempt scan fix
    
    Adding script assemblies
    
    Trying to create new csproj files in client folder
    
    Added submodules
    
    removed bad csproj
    
    Added submodule reference to git with fork
    
    added submodule pull test
    
    added git path fix
    
    trying the path fix the opposite way
    
    added token
    
    Fixed input name
    
    simplified script
    
    trying with github token
    
    checking to make sure it was just the token
    
    Added submodules pull, removed file checking
    
    Copying PackageCache .dll files to ScriptAssemblies
    
    Processing .sln file
    
    Cleaned up code

---
+++ client/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Behaviour Working. Time to take Monday off!");
+        Debug.Log("Submodule version: " + SubmoduleTest.SubmoduleVersion());
+    }
+
+    // Update is called once per frame
+    // This function is here to give SonarCloud something to complain about
+    void Update()
+    {
+        
+    }
+}
