78e83a9503aaced4f6306885a9aea3a9418931bc
Sean Simpson (ESG) <ssimpsonesg@Seans-MacBook-Pro.local>
2022-09-29 12:04:55 -0300

    Cleanup before demo

--- client/Assets/Scripts/HelloWorld.cs
+++ client/Assets/Scripts/HelloWorld.cs
@@ -7,9 +7,6 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // removed commented code
-        Debug.Log("Submodule version: " + SubmoduleTest.SubmoduleVersion());
-        Debug.Log("Adding some code to merge");
-        Debug.Log("So what happens now?");
+        Debug.Log("Submodule version: " + SubmoduleTest.SubmoduleVersion());        
     }
 }
